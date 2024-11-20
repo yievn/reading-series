767e604f62493f1a8b18cb3c041287a6971b9301
yewen.hu <yewen.hu@qlchat.com>
2024-11-20 16:36:16 +0800

    新增vant代码

---
+++ vant/packages/vant/src/overlay/Overlay.tsx
@@ -0,0 +1,101 @@
+import {
+  ref,
+  defineComponent,
+  Teleport,
+  Transition,
+  type PropType,
+  type CSSProperties,
+  type ExtractPropTypes,
+  type TeleportProps,
+} from 'vue';
+
+// Utils
+import {
+  isDef,
+  extend,
+  truthProp,
+  numericProp,
+  unknownProp,
+  preventDefault,
+  createNamespace,
+  getZIndexStyle,
+} from '../utils';
+
+// Composables
+import { useEventListener } from '@vant/use';
+import { useLazyRender } from '../composables/use-lazy-render';
+
+const [name, bem] = createNamespace('overlay');
+
+export const overlayProps = {
+  show: Boolean,
+  zIndex: numericProp,
+  duration: numericProp,
+  className: unknownProp,
+  lockScroll: truthProp,
+  lazyRender: truthProp,
+  customStyle: Object as PropType<CSSProperties>,
+  teleport: [String, Object] as PropType<TeleportProps['to']>,
+};
+
+export type OverlayProps = ExtractPropTypes<typeof overlayProps>;
+
+export default defineComponent({
+  name,
+
+  props: overlayProps,
+
+  setup(props, { slots }) {
+    const root = ref<HTMLElement>();
+    const lazyRender = useLazyRender(() => props.show || !props.lazyRender);
+
+    const onTouchMove = (event: TouchEvent) => {
+      if (props.lockScroll) {
+        preventDefault(event, true);
+      }
+    };
+
+    const renderOverlay = lazyRender(() => {
+      const style: CSSProperties = extend(
+        getZIndexStyle(props.zIndex),
+        props.customStyle,
+      );
+
+      if (isDef(props.duration)) {
+        style.animationDuration = `${props.duration}s`;
+      }
+
+      return (
+        <div
+          v-show={props.show}
+          ref={root}
+          style={style}
+          class={[bem(), props.className]}
+        >
+          {slots.default?.()}
+        </div>
+      );
+    });
+
+    // useEventListener will set passive to `false` to eliminate the warning of Chrome
+    useEventListener('touchmove', onTouchMove, {
+      target: root,
+    });
+
+    return () => {
+      const Content = (
+        <Transition
+          v-slots={{ default: renderOverlay }}
+          name="van-fade"
+          appear
+        />
+      );
+
+      if (props.teleport) {
+        return <Teleport to={props.teleport}>{Content}</Teleport>;
+      }
+
+      return Content;
+    };
+  },
+});
---
+++ vant/packages/vant/src/uploader/index.ts
@@ -0,0 +1,21 @@
+import { withInstall } from '../utils';
+import _Uploader, { UploaderProps } from './Uploader';
+
+export const Uploader = withInstall(_Uploader);
+export default Uploader;
+export { uploaderProps } from './Uploader';
+export type { UploaderProps };
+export type {
+  UploaderInstance,
+  UploaderThemeVars,
+  UploaderResultType,
+  UploaderFileListItem,
+  UploaderBeforeRead,
+  UploaderAfterRead,
+} from './types';
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    VanUploader: typeof Uploader;
+  }
+}
